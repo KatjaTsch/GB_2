c54365180ee064db2d4be028bba44f0695e6a876
eJwtzLFuwjAQgOF3udlDmzhJ8dqtSLQViMJkOecztWxi95wAEeLdK6Gsv/T9dzCIVMouBRpAAa3q1qJFWTkpjcO3vu06h9RWrlohNk5K6pr+BQRkThdviRd6X056XFan36Q3W7zU79uYj+FYt58fcf9zkHMZ16+bJpf6e/r6CyDgSfQ4ZwIFPRkmBgEF0zMYe/aDylMfPepAs2AyViU+CaacxFSIFZ2Nj+KaOLiYriCAbtkzFW1GUMMU4+PxDyxgUUc= <katjatschebotar.er@mail.ru>
2024-04-29 17:18:07 +0300

    HA

--- 4_JavaScript_Continuation/HW_5/5.js
+++ 4_JavaScript_Continuation/HW_5/5.js
@@ -5,28 +5,36 @@
 // 4. Добавить данный контент в вёрстку.
 // 5. * Добавить стили при необходимости (по желанию).
 
-// console.log(dataInfo);
-const containerEl = document.querySelector("div.container");
-console.log(containerEl);
+// // console.log(dataInfo);
+// const containerEl = document.querySelector("div.container");
+// console.log(containerEl);
 
-const parseResult = JSON.parse(dataInfo);
+// const parseResult = JSON.parse(dataInfo);
 
-parseResult.forEach((element) => {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add(element.class);
-  const imgEl = document.createElement("img");
-  imgEl.setAttribute("src", element.url);
-  imgEl.setAttribute("width", element.width);
-  imgEl.setAttribute("hight", element.hight);
+// parseResult.forEach((element) => {
+//   const newDiv = document.createElement("div");
+//   newDiv.classList.add(element.class);
+//   const imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", element.url);
+//   imgEl.setAttribute("width", element.width);
+//   imgEl.setAttribute("hight", element.hight);
 
-  const pEl = document.createElement("p");
-  pEl.innerHTML = element.text;
+//   const pEl = document.createElement("p");
+//   pEl.innerHTML = element.text;
 
-  const h1El = document.createElement("h1");
-  h1El.innerHTML = element.h1;
+//   const h1El = document.createElement("h1");
+//   h1El.innerHTML = element.h1;
 
-  newDiv.appendChild(imgEl);
-  newDiv.appendChild(pEl);
-  newDiv.appendChild(h1El);
-  containerEl.appendChild(newDiv)
-});
+//   newDiv.appendChild(imgEl);
+//   newDiv.appendChild(pEl);
+//   newDiv.appendChild(h1El);
+//   containerEl.appendChild(newDiv)
+// });
+
+fetch("https://api.nbrb.by/exrates/rates?periodicity=0")
+  .then((response) => response.json())
+  .then((parseResult) => {
+    parseResult.forEach(element => {
+        console.log(element);
+    });
+  });
--- 4_JavaScript_Continuation/HW_5/data.js
+++ 4_JavaScript_Continuation/HW_5/data.js
@@ -1,28 +1,28 @@
-const dataInfo = `
-[
-  {
-    "class": "img__container",
-    "url": "https://www.genevadrivingschool.ch/wp-content/uploads/2018/11/1-1.jpg",
-    "width": 300,
-    "hight": 350,
-    "h1": "Заголовок",
-    "text": "Картинка 1"
-  },
-  {
-    "class": "img__container",
-    "url": "https://благо-дарю.укр/wp-content/uploads/2018/10/cifra_2_5-225x300.jpg",
-    "width": 200,
-    "hight": 250,
-    "h1": "Заголовок",
-    "text": "Картинка 2"
-  },
-  {
-    "class": "img__container",
-    "url": "https://lerua.katalog-ceny.ru/image/cache/catalog/lerua2/cifra-3-duckanddog-malenkaya-70h65-mm-stal-cvet-chernij-15632764-200x200.jpg",
-    "width": 350,
-    "hight": 350,
-    "h1": "Заголовок",
-    "text": "Картинка 3"
-  }
-]
-`
+// const dataInfo = `
+// [
+//   {
+//     "class": "img__container",
+//     "url": "https://www.genevadrivingschool.ch/wp-content/uploads/2018/11/1-1.jpg",
+//     "width": 300,
+//     "hight": 350,
+//     "h1": "Заголовок",
+//     "text": "Картинка 1"
+//   },
+//   {
+//     "class": "img__container",
+//     "url": "https://благо-дарю.укр/wp-content/uploads/2018/10/cifra_2_5-225x300.jpg",
+//     "width": 200,
+//     "hight": 250,
+//     "h1": "Заголовок",
+//     "text": "Картинка 2"
+//   },
+//   {
+//     "class": "img__container",
+//     "url": "https://lerua.katalog-ceny.ru/image/cache/catalog/lerua2/cifra-3-duckanddog-malenkaya-70h65-mm-stal-cvet-chernij-15632764-200x200.jpg",
+//     "width": 350,
+//     "hight": 350,
+//     "h1": "Заголовок",
+//     "text": "Картинка 3"
+//   }
+// ]
+// `
