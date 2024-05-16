a49ff742dc82e8abdba4afc4c33d9c1fa350e5c1
eJwtzLFuwjAQgOF3udlDmzhJ8dqtSLQViMJkOecztWxi95wAEeLdK6Gsv/T9dzCIVMouBRpAAa3q1qJFWTkpjcO3vu06h9RWrlohNk5K6pr+BQRkThdviRd6X056XFan36Q3W7zU79uYj+FYt58fcf9zkHMZ16+bJpf6e/r6CyDgSfQ4ZwIFPRkmBgEF0zMYe/aDylMfPepAs2AyViU+CaacxFSIFZ2Nj+KaOLiYriCAbtkzFW1GUMMU4+PxDyxgUUc= <katjatschebotar.er@mail.ru>
2024-05-16 16:36:53 +0300

    HW

---
+++ 4_JavaScript_Continuation/S_6/6.js
@@ -0,0 +1,48 @@
+const videoEl = document.createElement("video");
+videoEl.setAttribute("src", "media/v_1.mp4");
+
+const containerEl = document.querySelector(".containerVideo");
+containerEl.appendChild(videoEl);
+
+const playEl = document.createElement("div");
+const pauseEl = document.createElement("div");
+playEl.classList.add("play");
+pauseEl.classList.add("pause");
+
+playEl.addEventListener("click", function (e) {
+  videoEl.play();
+  console.log(videoEl.duration, videoEl.currentTime);
+});
+pauseEl.addEventListener("click", function (e) {
+  videoEl.pause();
+  console.log(videoEl.duration, videoEl.currentTime);
+});
+
+const rangeEl = document.createElement("input");
+rangeEl.setAttribute("type", "range");
+rangeEl.setAttribute("min", "0");
+rangeEl.setAttribute("max", "100");
+rangeEl.setAttribute("value", "0");
+
+rangeEl.addEventListener("change", function (e) {
+  console.log(e.target.value);
+  videoEl.currentTime = (e.target.value / 100) * videoEl.duration;
+});
+
+videoEl.addEventListener("timeupdate", (e) => {
+  rangeEl.setAttribute(
+    "value",
+    Math.round(videoEl.currentTime / videoEl.duration) * 100
+  );
+});
+
+const volumeEl = document.createElement("input");
+volumeEl.setAttribute("type", "range");
+volumeEl.setAttribute("min", "0");
+volumeEl.setAttribute("max", "100");
+volumeEl.setAttribute("value", "0");
+
+containerEl.appendChild(volumeEl);
+containerEl.appendChild(rangeEl);
+containerEl.appendChild(playEl);
+containerEl.appendChild(pauseEl);
